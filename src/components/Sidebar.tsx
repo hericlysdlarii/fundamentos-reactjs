74bf6753b6119417074bbe8cbd204c2d16b2b3e8
hericlysdlarii <hericlysdlarii@gmail.com>
2023-09-15 00:28:50 -0300

    Componente: Comment

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,29 +3,29 @@ import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
-    return(
-        <aside className={styles.sidebar}>
-            <img 
-                className={styles.cover} 
-                src="https://images.unsplash.com/photo-1599837826158-79e79f6e032f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" 
-            />
+  return(
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover} 
+        src="https://images.unsplash.com/photo-1599837826158-79e79f6e032f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" 
+      />
 
-            <div className={styles.profile}>
-                <img 
-                    className={styles.avatar} 
-                    src="https://github.com/hericlysdlarii.png" 
-                />
+      <div className={styles.profile}>
+        <img 
+          className={styles.avatar} 
+          src="https://github.com/hericlysdlarii.png" 
+        />
 
-                <strong>Hériclys D'Larii</strong>
-                <span>Web Developer</span>
-            </div>
+        <strong>Hériclys D'Larii</strong>
+        <span>Web Developer</span>
+      </div>
 
-            <footer>
-                <a href="#">
-                    <PencilLine size={20} />
-                    Editar seu perfil
-                </a>
-            </footer>
-        </aside>
-    );
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
 }
