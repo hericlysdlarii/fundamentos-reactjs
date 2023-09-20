0d2109df006a0ba9963836ae3f433a8d403228bd
hericlysdlarii <hericlysdlarii@gmail.com>
2023-09-19 22:15:58 -0300

    Iterando no JSX

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,35 @@ import styles from './App.module.css';
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/hericlysdlarii.png',
+      name: 'Hériclys DLarii',
+      role: 'Web Developer',
+    },
+    content:[
+      {type: 'paragraph', content: 'E aí, galera!!!'},
+      {type: 'paragraph', content: 'Este é o meu post na rede social.'},
+    ],
+    publishedAt: new Date('2023-09-19 21:58:00'),
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/hericlysdlarii.png',
+      name: 'Hériclys DLarii',
+      role: 'Escravo',
+    },
+    content:[
+      {type: 'paragraph', content: 'E aí, galera!!!'},
+      {type: 'paragraph', content: 'Este é o meu post na rede social.'},
+    ],
+    publishedAt: new Date('2023-10-20 22:00:00'),
+  },
+]
 
 export function App() {
   return (
@@ -15,14 +44,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-          author="Héricls D'Larii" 
-          content="Blá Blá Blá"
-          />
-          <Post 
-          author="Héricls D'Larii" 
-          content="Blá Blá Blá"
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
